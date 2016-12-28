f9695ac4d09d799d60a8d656b6db0fe1c8ee33e6
vlad-chikileyski <vlad.chikileyski@gmail.com>
2016-12-28 14:01:10 +0100

    v1.0
    v1.0

---
+++ iStudents ABC/MyPostPage.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.WindowsAzure.MobileServices;
+using Microsoft.Phone.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows.Documents;
+
+namespace iStudents_ABC
+{
+    public partial class MyPostPage : PhoneApplicationPage
+    {
+                private MobileServiceCollection<post, post> items;
+
+                string ID_USER_ASIC;
+
+        public MyPostPage()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void btnPrevPage_Click(object sender, RoutedEventArgs e)
+        {
+            GoToFirstView();
+        }
+
+        private void GoToFirstView()
+        {
+            
+            NavigationService.Navigate(new Uri("/DashBoardPage.xaml", UriKind.RelativeOrAbsolute)); 
+        }
+
+        private async void myListView_Loaded(object sender, RoutedEventArgs e)
+        {
+       
+            MobileServiceInvalidOperationException exception = null;
+            try
+            {
+                NavigationContext.QueryString.TryGetValue("ID_USER_ASIC", out ID_USER_ASIC);
+
+                items = await App.MobileService.GetTable<post>().Where(todoItem => todoItem.id == ID_USER_ASIC).ToCollectionAsync();
+                post listitem = myListView.ItemsSource as post;
+                var firstFromGroup = items.First();
+               txtValid_id.Text =  firstFromGroup.id;
+               txtValid_title.Text = firstFromGroup.title;
+               txtxValid_status.Text = firstFromGroup.status;
+
+                try
+                {
+                    OnMessageReceived(firstFromGroup.description);
+                    txtDisplayText = null;
+                }
+                catch { }
+                finally { 
+             
+                }   
+            }
+            catch (MobileServiceInvalidOperationException ex)
+            {
+               exception = ex;
+            }
+
+            if (exception == null) myListView.DataContext = items;
+           
+        }
+        private void OnMessageReceived(string message)
+        {
+            var paragraph = new Paragraph();
+
+            var runs = new List<Inline>();
+
+            string[] wordArray = message.Split(' ', '\r');
+
+            foreach (var word in wordArray)
+            {
+                Regex emailid = new Regex(@"^[A-Za-z0-9_\-\.]+@(([A-Za-z0-9\-])+\.)+([A-Za-z\-])+$");
+                Regex phonenumber = new Regex(@"^(?:\(?([0-9]{3})\)?[-. ]?)?([0-9]{3})[-. ]?([0-9]{4})$");
+                Uri uri;
+                if (emailid.IsMatch(word))
+                {
+                    var link = new Hyperlink();
+                    link.Inlines.Add(new Run() { Text = word });
+                    link.Click += (sender, e) =>
+                    {
+                        var hyperLink = (sender as Hyperlink);
+                        new EmailComposeTask() { To = word }.Show();
+                    };
+                    runs.Add(link);
+
+                }
+
+                else if (Uri.TryCreate(word, UriKind.Absolute, out uri) ||
+                   (word.StartsWith("www.") || word.EndsWith(".com")) && Uri.TryCreate("http://" + word, UriKind.Absolute, out uri))
+                {
+                    var link = new Hyperlink();
+                    link.Inlines.Add(new Run() { Text = word });
+                    link.Click += (sender, e) =>
+                    {
+                        var hyperLink = (sender as Hyperlink);
+                        new WebBrowserTask() { Uri = uri }.Show();
+                    };
+
+                    runs.Add(link);
+                }
+                else if (phonenumber.IsMatch(word))
+                {
+                    var link = new Hyperlink();
+                    link.Inlines.Add(new Run() { Text = word });
+                    link.Click += (sender, e) =>
+                    {
+                        var hyperLink = (sender as Hyperlink);
+                        new PhoneCallTask() { PhoneNumber = word }.Show();
+                    };
+
+                    runs.Add(link);
+                }
+                else
+                {
+                    runs.Add(new Run() { Text = word });
+                }
+
+                runs.Add(new Run() { Text = " " });
+            }
+
+            foreach (var run in runs)
+                paragraph.Inlines.Add(run);
+
+            txtDisplayText.Blocks.Add(paragraph);
+            
+        }
+      
+
+       
+        
+        
+    }
+}
---
+++ iStudents ABC/NewPostPage.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Media;
+using Microsoft.Phone.Tasks;
+using System.Windows.Media.Imaging;
+
+
+namespace iStudents_ABC
+{
+    public partial class NewPostPage : PhoneApplicationPage
+    {
+        public NewPostPage()
+        {
+            InitializeComponent();
+        }
+        void TaskCompletedPhoto(object sender, PhotoResult e)
+        {
+            if (e.TaskResult == TaskResult.OK)
+            {
+                BitmapImage bmp = new BitmapImage();
+                bmp.SetSource(e.ChosenPhoto);
+                imageMakePhotoGALERIA.Source = bmp;
+                btnGallery.IsEnabled = false;
+                btnMakePhoto.IsEnabled = false;
+                btnDelImage.Visibility = Visibility.Visible;
+                
+            }
+        }
+
+        void TaskCompletedGallery(object sender, PhotoResult e)
+        {
+            if (e.TaskResult == TaskResult.OK)
+            {
+                BitmapImage bmp = new BitmapImage();
+                bmp.SetSource(e.ChosenPhoto);
+                imageMakePhoto.Source = bmp;
+                btnGallery.IsEnabled = false;
+               btnMakePhoto.IsEnabled = false;
+               btnDelImageG.Visibility = Visibility.Visible;
+
+            }
+        }
+
+
+        private void btnAddValidate(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPrevPage_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/DashBoardPage.xaml", UriKind.RelativeOrAbsolute)); 
+
+        }
+
+        private void txtDesc_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtBDesc.Text = "";
+        }
+
+        private void txtDesc_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtBDesc.Text)) { txtBDesc.Text = "Write a description"; }
+
+        }
+
+        private void btnMakePhoto_Click(object sender, RoutedEventArgs e)
+        {
+            var cameraCaptureTask = new CameraCaptureTask();
+            cameraCaptureTask.Completed += TaskCompletedPhoto;
+            cameraCaptureTask.Show();
+           
+        }
+
+        private void btnGallery_Click(object sender, RoutedEventArgs e)
+        {
+            var photoChooserTask = new PhotoChooserTask();
+            photoChooserTask.Completed += TaskCompletedGallery;
+            photoChooserTask.Show();
+        }
+
+        private void btnDelImage_Click(object sender, RoutedEventArgs e)
+        {
+            imageMakePhotoGALERIA.Source = null;
+            btnGallery.IsEnabled = true;
+            btnMakePhoto.IsEnabled = true;
+            Visibility = Visibility.Visible;
+            btnDelImage.Visibility = Visibility.Collapsed;
+        }
+
+        private void btnDelImageG_Click(object sender, RoutedEventArgs e)
+        {
+            imageMakePhoto.Source = null;
+            btnGallery.IsEnabled = true;
+            btnMakePhoto.IsEnabled = true;
+            btnDelImageG.Visibility = Visibility.Collapsed;
+
+        }
+        private void txtBTitle_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtBTitle.Text = "";
+
+        }
+        private void txtBTitle_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtBTitle.Text)) { txtBTitle.Text = "Title"; }
+           
+        }
+
+        private async void btnAddNewPost(object sender, RoutedEventArgs e)
+        {
+            
+                post item = new post
+                {
+                    title = txtBTitle.Text,
+                    status = txtBStatus.Text,
+                    description = txtBDesc.Text
+
+
+                };
+                await App.MobileService.GetTable<post>().InsertAsync(item);
+                NavigationService.Navigate(new Uri("/DashBoardPage.xaml", UriKind.RelativeOrAbsolute));
+           
+        }
+
+        private void txtBStatus_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtBStatus.Text = "";
+        }
+
+        private void txtBStatus_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtBStatus.Text)) { txtBStatus.Text = "Status"; }
+
+        }
+
+       
+    }
+}
